d6dcdd2a59b77aa0cf91cd0593e58fc164f0c1d7
Xiuzhi Chen <xche0042@student.monash.edu>
2019-04-25 12:03:37 +1000

    Updated new API for getting average rental price for areas

--- CoachMe/Controllers/ValuesController.cs
+++ CoachMe/Controllers/ValuesController.cs
@@ -78,5 +78,51 @@ namespace CoachMe.Controllers
             // Return results for the GET api
             return results;
         }
+
+        // GET api/values/
+        public List<Results> Get()
+        {
+            // Create connection for MySQL database
+            MySqlConnection conn = WebApiConfig.conn();
+
+            // Define the MySQL Statement to query all the areas within the price range
+            MySqlCommand query = conn.CreateCommand();
+            query.CommandText = "SELECT s.suburb_name, s.suburb_lat, " +
+                "s.suburb_long, AVG(NULLIF(pr.median_price, 0)) AS median_price " +
+                "FROM suburb s JOIN suburb_property sp ON s.suburb_id = sp.suburb_id " +
+                "JOIN property p ON p.property_id = sp.property_id " +
+                "JOIN property_rental pr ON sp.suburb_property_id = pr.suburb_property_id " +
+                "GROUP BY s.suburb_name;";
+
+            // Use prefined Result class to store the values returned
+            var results = new List<Results>();
+
+            // Error Handling 
+            try
+            {
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                results.Add(new Results(null, null, null, null, ex.ToString()));
+                results.Add(new Results(null, null, null, null, "No Connection!"));
+            }
+
+            // Define the fetch query
+            MySqlDataReader fetch_query = query.ExecuteReader();
+
+            // Parse the query result and store in the Result list
+            while (fetch_query.Read())
+            {
+                results.Add(new Results(fetch_query["suburb_name"].ToString()
+                    , fetch_query["suburb_lat"].ToString()
+                    , fetch_query["suburb_long"].ToString()
+                    , fetch_query["median_price"].ToString()
+                    , null));
+            }
+
+            // Return results for the GET api
+            return results;
+        }
     }
 }
